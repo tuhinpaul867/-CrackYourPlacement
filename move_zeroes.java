87557dbb15edcdcec3b466a85f8504abe6f3a33e
Tuhin Paul <131508789+tuhinpaul867@users.noreply.github.com>
2024-07-23 11:15:14 +0530

    Add files via upload

---
+++ move_zeroes.java
@@ -0,0 +1,20 @@
+public class move_zeroes {
+   
+        public void moveZeroes(int[] nums) {
+            int j = 0;
+           for(int i=0; i<nums.length; i++){
+            if(nums[i] !=0){
+                int temp = nums[i];
+                nums[i] = nums [j];
+                nums[j] = temp;
+                j++;
+            }
+           }
+           
+           for(int i = j; i<nums.length; i++){
+            nums[i]=0;
+           }
+           
+        }
+    }
+
---
+++ remove_duplicate_from_sorted_array.java
@@ -0,0 +1,15 @@
+class remove_duplicates_from_sorted_array {
+    public int removeDuplicates(int[] nums) {
+
+        int n = nums.length;
+
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (nums[j] != nums[i]) {
+                nums[i + 1] = nums[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+}
